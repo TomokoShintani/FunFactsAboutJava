7b51de8574bdf71d38b5da8e1dcc0bd18398abab
tomoko <tom.shintani@gmail.com>
2024-02-23 18:01:35 +0900

    Add Main.java

--- Polymorphism/src/Main.java
+++ Polymorphism/src/Main.java
@@ -6,5 +6,15 @@ public class CompileTimeExample {
         // m(int x) should also be defined in the apparent type of temp not only the actual type.
         temp.m();
         temp.m(2);
+
+        Object obj = new RuntimeChild();
+
+        ((RuntimeParent) obj).age = 56;
+        ((RuntimeParent) obj).name = "Bob";
+        ((RuntimeParent) obj).m();
+        // Upcasting Child object to Parent object.
+        // obj ---> apparent type:Parent  actual type:Child
+        // obj got access to Parent instance variable 'age' and lost access to Child instance variable 'name'.
+        // But this code executes m() in Child because the actual type of obj is Child.
     }
 }
